adefca83dc8dad9e2a9d705b4c5e070ccc80e5db
Matiasdf <matiasdferrero@gmail.com>
2024-05-06 00:19:31 -0300

    Intento de sprint 1 -> 3 y 4 Hist. Us.

--- app/page.tsx
+++ app/page.tsx
@@ -1,127 +1,170 @@
 'use client'
 import React, {useEffect, useState} from "react";
-import ButtonGrid from "./componentes/bto-base";
 import {getRecursoList, type Recurso} from "./service/recursos"
 import './Css/pantalla.css'
-
-
-//---------------------------------------------------------------------
-// parte superior recursos y botones desplegables
-//---------------------------------------------------------------------
-const HorizontalList: React.FC<{ items: Recurso[] }> = ({ items }) => {
-  return (
-    <div className="horizontal-list-container"> {/* Estilo de Flexbox */}
-      {items.map((item) => (
-        <div key={item.id} className="list-item"> {/* Cada par "nombre: cantidad" */}
-          <span className="item-text">
-            {item.name} : {item.cantidad}
-          </span>
-        </div>
-      ))}
-    </div>
-  );
-};
-// botones alineados
-const ButtonGroup: React.FC<{ buttons: { text: string, onClick: () => void }[] }> = ({ buttons }) => {
-  return (
-    <div className="button-group-container"> {/* Contenedor para centrar y separar botones */}
-      {buttons.map((button, index) => (
-        <button
-          key={index}
-          className="button-item"
-          onClick={button.onClick}
-        >
-          {button.text} {/* Texto del botón */}
-        </button>
-      ))}
-    </div>
-  );
-};
+import { Edificios, findEdificiosById, getEdificiosList } from "./service/edificios-menu";
 
 //---------------------------------------------------------------------
-// Componente que muestra el círculo grande y otros círculos pequeños
+// botones edificios
 //---------------------------------------------------------------------
-const CircleComponent: React.FC = () => {
-  const numberOfCircles = 6; // Número de círculos pequeños
-  const radius = 100; // Radio del círculo grande
+export default function Map() {
+  /* Zoom al mapa, sin uso todavía
+  const mapRef = useRef<HTMLDivElement>(null);
+  const zoomRef = useRef<number>(1);
 
-  // Distribuye los círculos pequeños alrededor del círculo grande
-  const circles = Array.from({ length: numberOfCircles }, (_, index) => {
-    const angle = (2 * Math.PI * index) / numberOfCircles; // Ángulo para cada círculo
-    const x = Math.cos(angle) * radius; // Posición X
-    const y = Math.sin(angle) * radius; // Posición Y
+  const handleWheel = (e: WheelEvent<HTMLDivElement>) => {
+    const deltaY = e.deltaY;
+    zoomRef.current += deltaY > 0 ? -0.1 : 0.1;
 
+    if (mapRef.current) {
+      mapRef.current.style.transform = scale(${zoomRef.current});
+    }
+  };*/
+
+//---------------------------------------------------------------------
+// parte superior recursos 
+//---------------------------------------------------------------------
+  const HorizontalList: React.FC<{ items: Recurso[] }> = ({ items }) => {
     return (
-      <div
-        key={index}
-        className="small-circle"
-        style={{ transform: `translate(${x}px, ${y}px)` }} // Posicionar cada círculo
-      >
-        {/* Aquí puedes poner una imagen o cualquier contenido */}
-        {index + 1} {/* Número del círculo */}
-      </div>
-    );
+      <div className="horizontal-list-container p-2"> {/* Estilo de Flexbox */}
+        {items.map((item) => (
+         <div key={item.id} className="list-item"> {/* Cada par "nombre: cantidad" */}
+           <span className="item-text">
+             {item.name} : {item.cantidad}
+           </span>
+         </div>
+       ))}
+     </div>
+   );
+  };
+
+//---------------------------------------------------------------------
+//aca muestra todo lo de recursos
+//---------------------------------------------------------------------
+  
+const [recursos, setRecursos] = useState<Recurso[]>([]); // Estado para la lista
+const [showCircle, setShowCircle] = useState(false); // Estado para mostrar el círculo grande
+const [edificios, setEdificios] = useState<Edificios[]>([]);
+
+useEffect(() => {
+  const fetchData = async () => {
+    const result = await getRecursoList(); // Obtener datos
+    const edificios = await getEdificiosList(); // Obtener lista de edificios
+    setRecursos(result.list); // Guardar datos en el estado
+    setEdificios(edificios.list) // Guardar edificios en su estado
+  };
+
+  fetchData(); // Ejecuta para obtener datos al montar el componente
+}, []); // Efecto se ejecuta una vez
+
+//--- Edificios --------------------------------------------------
+const [buildingImages, setBuildingImages] = useState(Array.from({ length: 3 }).fill(''));
+const [showBuildMenu, setShowBuildMenu] = useState(false)
+const [showConstruir, setShowConstruir] = useState(false)
+const [selectedBuilding, setSelectedBuilding] = useState<Edificios>()
+const [selectedGround, setSelectedGround] = useState(Number)
+//const pozo_image = 'url(/placeholders/building1_ph.png)'
+
+const BuildingsMenu: React.FC<{ items: Edificios[], onItemClick: (index: number) => void }> = ({ items, onItemClick }) => {
+  return (
+    <div className=" p-5"> {/* Estilo de Flexbox */}
+      {items.map((item, index) => (
+       <div 
+        key={item.id} 
+        className="item-text bg-black cursor-pointer hover:bg-opacity-50"
+        onClick={() => onItemClick(index)}
+      > {/* Cada par "nombre: descripcion" */}
+          {item.name} : {item.descripcion}
+       </div>
+     ))}
+   </div>
+ );
+};
+
+const handleItemClick = (index: number) => {
+  setShowConstruir(true)
+  setSelectedBuilding(edificios[index])
+  console.log(selectedBuilding)
+  //console.log(edificios[index]);
+}
+
+const handleConstruirClick = (index : number) => {
+  const newBuildingImages = [...buildingImages]
+  newBuildingImages[index] = newBuildingImages[index] === '' ?  selectedBuilding?.imagen: '';
+  setBuildingImages(newBuildingImages)
+  setShowBuildMenu(false)
+}
+
+//--- Lógica de edificios ------------------------------------------------
+  
+//--- Estados de edificios--- 
+// El edificio de la base  
+  const base_building = { 
+    style: { backgroundImage: 'url(/placeholders/base_ph.png)' },
+    className: 'h-48 w-48 bg-cover bg-opacity-0 hover:bg-opacity-5 cursor-pointer',
+    //onClick: handleClick
+  };
+
+  // El terreno vacio
+  const empty_ground = (index: number) => ({
+    style: { backgroundImage: buildingImages[index] },
+    className: 'h-48 w-48 bg-cover bg-white bg-opacity-0 cursor-pointer hover:bg-opacity-10',
+    onClick: () => handleEmptyGroundClick(index)
   });
 
-  return (
-    <div className="large-circle"> {/* Gran círculo que contiene los pequeños */}
-      {circles} {/* Coloca los círculos pequeños */}
-    </div>
-  );
-};
-//botones grnades y pequeños
-//---------------------------------------------------------------------
+  // El terreno con un edificio construido
+  const building = (index: number) => ({
+    style: { backgroundImage: buildingImages[index] },
+    className: 'h-48 w-48 bg-cover bg-white bg-opacity-0 cursor-pointer hover:bg-opacity-10', 
+    //onClick: () => handleClick(index)
+  });
 
+  const handleEmptyGroundClick = (index: number) => {
+    setShowBuildMenu(true)
+    setSelectedGround(index)
+    /*const newBuildingImages = [...buildingImages];
+    newBuildingImages[index] = newBuildingImages[index] === '' ? 'url(/placeholders/building1_ph.png)' : '';
+    setBuildingImages(newBuildingImages);*/
+  };
 
-//aca muestra todo
-const App: React.FC = () => {
-  const [recursos, setRecursos] = useState<Recurso[]>([]); // Estado para la lista
-  const [showCircle, setShowCircle] = useState(false); // Estado para mostrar el círculo grande
+  const buildings = Array.from({ length: 3 }).map((_, index) => {
+    if (index === 1) {
+      return <div key={index} style={base_building.style} className={base_building.className}></div>;
+    } else {
+      return <div key={index} style={(empty_ground(index).style as React.CSSProperties)} className={empty_ground(index).className} onClick={() => handleEmptyGroundClick(index)}></div>;
+    }
+  });
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await getRecursoList(); // Obtener datos
-      setRecursos(result.list); // Guardar datos en el estado
-    };
-
-    fetchData(); // Ejecuta para obtener datos al montar el componente
-  }, []); // Efecto se ejecuta una vez
-
-  // Botones que pueden controlar la visibilidad del círculo grande
-  const buttons = [
-    {
-      text: 'Botón 1',
-      onClick: () => {
-        console.log('Botón 1 clickeado');
-        setShowCircle(!showCircle); // Alterna mostrar/ocultar el círculo
-      },
-    },
-    {
-      text: 'Botón 2',
-      onClick: () => {
-        console.log('Botón 2 clickeado');
-        setShowCircle(!showCircle); // Alterna mostrar/ocultar el círculo
-      },
-    },
-    {
-      text: 'Botón 3',
-      onClick: () => {
-        console.log('Botón 3 clickeado');
-        setShowCircle(!showCircle); // Alterna mostrar/ocultar el círculo
-      },
-    },
-  ];
-
-  return (
-    <div>
-      <HorizontalList items={recursos} /> {/* Mostrar la lista */}
-      <ButtonGroup buttons={buttons} /> {/* Mostrar los botones */}
-      {showCircle && <CircleComponent />} {/* Mostrar el círculo grande si está activo */}
-    </div>
-  );
-};
-//---------------------------------------------------------------------
-//---------------------------------------------------------------------
-
-
-export default App;
+  //---- Pantalla --------------------------------------------
+    return(
+      <main>
+      {/* Contenedor principal que ocupa toda la altura y anchura de la pantalla */}
+      <div className="h-screen w-screen bg-slate-900 flex flex-col"> 
+        {/* Sección superior para mantener el HorizontalList en la parte superior izquierda */}
+        <div className="flex justify-start items-start"> 
+          <HorizontalList items={recursos} /> {/* Mostrar la lista en la parte superior izquierda */}
+        </div>
+        {/* Sección inferior para el resto del contenido, centrado horizontalmente y abajo */}
+        <div className="flex flex-1 flex-col justify-end items-center"> 
+          <div className="flex flex-row">
+            {buildings} {/* Mostrar edificios */}
+          </div>
+          <div className="h-48 w-screen bg-slate-600">
+          {showBuildMenu &&
+            <div className="h-48 w-1/2 bg-white">
+              <BuildingsMenu items={edificios} onItemClick={handleItemClick}/>
+              {showConstruir && 
+              <div className="flex flex-row justify-end items-end">
+                <div className="bg-black m-3 px-2 py-2 cursor-pointer hover:bg-opacity-50" onClick={()=>(handleConstruirClick(selectedGround))}>Construir</div>
+                <div className="bg-black m-3 px-2 py-2 cursor-pointer hover:bg-opacity-50" onClick={() => (setShowBuildMenu(false))}>Cancelar</div>
+              </div>
+              }
+            </div>
+            }
+          </div>
+        </div>
+      </div>
+    </main>
+    )
+  
+}
--- app/service/edificios-menu.ts
+++ app/service/edificios-menu.ts
@@ -3,11 +3,12 @@ export type Edificios = {
   id: number
   name: string
   descripcion: string
+  imagen: any
 }
 const recursoList: Edificios[] = [
-  { id: 1, name: 'Pozo', descripcion: "Recoleccion de agua"  },
-  { id: 2, name: 'Criadero', descripcion: "Matanza de ovejas radiactivas" },
-  { id: 3, name: 'Chatarreria', descripcion: "Recoleccion de chatarra" },
+  { id: 1, name: 'Pozo', descripcion: "Recoleccion de agua", imagen: 'url(placeholders/pozo_ph.png)'},
+  { id: 2, name: 'Criadero', descripcion: "Matanza de ovejas radiactivas", imagen: 'url(placeholders/criadero_ph.png)'},
+  { id: 3, name: 'Chatarreria', descripcion: "Recoleccion de chatarra", imagen: 'url(placeholders/chatarra_ph.png)'},
 ]
 
 export const findEdificiosById = async (id: number) => {
